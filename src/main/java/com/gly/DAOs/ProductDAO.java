2c76948617cde30a15864e36ecd86f9e3c86e86a
hansol <soool1021@gmail.com>
2022-11-24 16:59:26 +0900

    main작업

--- src/main/java/com/gly/DAOs/ProductDAO.java
+++ src/main/java/com/gly/DAOs/ProductDAO.java
@@ -1,15 +1,73 @@
 package com.gly.DAOs;
 
-//import java.sql.Connection;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//
-//import javax.naming.Context;
-//import javax.naming.InitialContext;
-//import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
 
 public class ProductDAO {
-	private ProductDAO() {};
-	private static ProductDAO instance = new ProductDAO();
-	public static ProductDAO getInstance() { return instance; }
+	private Connection con;
+	private PreparedStatement pstmt;
+	private DataSource dataFactory;
+
+	public ProductDAO() {
+		
+		try {
+			// 1. InitialContext 객체 생성: 톰켓 서버에서 자원을 찾는 InitialContext
+			Context initContext = new InitialContext();
+			// 2. 컨텍스트 객체의 lookup 메소드로 JNDI에 등록되있는 서버 자원을 찾음 DataSource ds = (DataSource)
+			// initialContext.lookup('java:/comp/env/jdbc/myoracle'); 과 동일
+			Context envContext = (Context) initContext.lookup("java:/comp/env");
+			dataFactory = (DataSource) envContext.lookup("jdbc/glyoracle");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void listNewProduct() {
+
+		try {
+			con = dataFactory.getConnection();
+			System.out.println("Connection success");
+			String query = "select img_src, p_name, p_price from main_img_list m, product p"
+					 + " where m.p_id=p.p_id and rownum<=4"
+					+" order by p_indate desc ";
+			pstmt = con.prepareStatement(query);
+			ResultSet rset = pstmt.executeQuery();
+			while (rset.next()) {
+				System.out.println(rset.getString(1));
+				System.out.println(rset.getString(2));
+				System.out.println(rset.getInt(3));
+			}
+			rset.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public void listBestProduct() {
+
+		try {
+			con = dataFactory.getConnection();
+			System.out.println("Connection success");
+			String query = "select img_src from (select * from images, product order by p_indate desc)"
+					 + "where rownum<=4 ";
+			pstmt = con.prepareStatement(query);
+			ResultSet rset = pstmt.executeQuery();
+			while (rset.next()) {
+				System.out.println(rset.getString(1));
+			}
+			rset.close();
+			pstmt.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
--- src/main/java/com/gly/controllerAction/IndexAction.java
+++ src/main/java/com/gly/controllerAction/IndexAction.java
@@ -1,19 +1,25 @@
 package com.gly.controllerAction;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.gly.DAOs.ProductDAO;
+
 public class IndexAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = "/main/main.jsp";
-		
+		String url = "main/main.jsp";
+		response.setContentType("text/html;charset=utf-8");
+		PrintWriter out=response.getWriter();	
+		ProductDAO dao = new ProductDAO();
+		dao.listBestProduct();
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
-		dispatcher.forward(request, response);
+//		dispatcher.forward(request, response);
 		
 	}
 
