0a2c8484b0268aa8bd9d44c9fafd73c642f62110
juhyemi <rlawngp124@gmail.com>
2022-11-27 19:27:17 +0900

    장바구니 담기 구현

--- src/main/java/com/gly/controllerAction/CartDeleteAction.java
+++ src/main/java/com/gly/controllerAction/CartDeleteAction.java
@@ -3,14 +3,33 @@ package com.gly.controllerAction;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
+
+import com.gly.DAOs.*;
+import com.gly.VOs.*;
 
 public class CartDeleteAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url="mypage/shoppingbag.jsp";
+		String url="/gly?command=cart_list";
+		
+		CartDAO cdDAO = CartDAO.getInstance();
+		
+		int dsize = Integer.parseInt(request.getParameter("dsize"));
+		String dcolor = request.getParameter("dcolor");
+		int dquantity = Integer.parseInt(request.getParameter("dquantity"));
+		int dp_id = Integer.parseInt(request.getParameter("p_id"));
+		HttpSession session = request.getSession();
+		MemberVO  userInfo = (MemberVO) session.getAttribute("userInfo");
+		String username = userInfo.getM_id();
+		
+		System.out.println(dsize);
+		System.out.println(dcolor);
+		System.out.println(dquantity);
+		
+		int pdid = cdDAO.getpdid(dsize, dcolor, dp_id);
+		cdDAO.deletecart(dquantity, username, pdid);
 		
 		request.getRequestDispatcher(url).forward(request, response);  
 	}
