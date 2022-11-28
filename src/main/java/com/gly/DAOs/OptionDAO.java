a35454f0df6da0c47a1ebed970973f4f4e633d37
HanulKim <hanscnnee@gmail.com>
2022-11-28 16:52:17 +0900

    fix: product package procedure)get_all_options

--- src/main/java/com/gly/DAOs/OptionDAO.java
+++ src/main/java/com/gly/DAOs/OptionDAO.java
@@ -10,9 +10,7 @@ import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.sql.DataSource;
 
-import com.gly.VOs.ImageVO;
 import com.gly.VOs.OptionVO;
-import com.gly.VOs.ProductVO;
 
 import oracle.jdbc.OracleTypes;
 
@@ -37,29 +35,39 @@ public class OptionDAO {
 	
 	
 	// writer : Hanul
-	public ArrayList<OptionVO> getProductOptions(int p_id) {
-		ArrayList<OptionVO>	options = new ArrayList<OptionVO>();
+	public ArrayList<ArrayList<OptionVO>> getProductOptions(int p_id) {
+		ArrayList<ArrayList<OptionVO>> options = new ArrayList<ArrayList<OptionVO>>();
 		// 상품 옵션 정보
-		String query = "{ call prod.get_all_options(?, ?)}";
+		String query = "{ call prod.get_all_options(?, ?, ?)}";
 		
 		try {
 			con = dataFactory.getConnection();
 			CallableStatement callableStatement = con.prepareCall(query);
-			callableStatement.setInt(1, p_id);			// p_kind 
+			callableStatement.setInt(1, p_id); 
 			callableStatement.registerOutParameter(2, OracleTypes.CURSOR);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
 			callableStatement.execute();
 
 			ResultSet rset = (ResultSet) callableStatement.getObject(2);
-
+			ArrayList<OptionVO> colorOption = new ArrayList<OptionVO>();
+			
 			// p_color, p_size, p_d_id
 			while (rset.next()) {
 				OptionVO option = new OptionVO();
 				option.setP_color(rset.getString("p_color"));
-				option.setP_szie(rset.getInt("p_size"));
-				option.setP_d_id(rset.getInt("p_d_id"));
-				options.add(option);
+				colorOption.add(option);
 			}
-			System.out.println();
+			
+			rset = (ResultSet) callableStatement.getObject(3);
+			ArrayList<OptionVO> sizeOption = new ArrayList<OptionVO>();
+			while (rset.next()) {
+				OptionVO option = new OptionVO();
+				option.setP_szie(rset.getInt("p_size"));
+				sizeOption.add(option);
+			}
+			
+			options.add(colorOption);
+			options.add(sizeOption);
 			
 			rset.close();
 			con.close();
