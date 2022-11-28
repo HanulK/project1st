a2ea00f67287cfdfb1b91b0077a0cf8d949dc34b
SY-GG <sy.park.HUB@gmail.com>
2022-11-28 20:35:24 +0900

    주문 후 내 리스트로 가기

--- src/main/java/com/gly/controllerAction/OrderInsertAction.java
+++ src/main/java/com/gly/controllerAction/OrderInsertAction.java
@@ -14,7 +14,7 @@ public class OrderInsertAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url="mypage/myorders.jsp";
+		String url="gly?command=order_list";
 		
 		
 		HttpSession session = request.getSession();
